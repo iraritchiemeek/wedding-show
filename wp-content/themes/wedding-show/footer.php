fae6ed23954fbcadf4833f356c7ad8b7a7fd0406
craig christensen <craigchristensen@craigs-MacBook-Pro-2.local>
2021-11-05 11:39:42 +1300

    so far

--- wp-content/themes/wedding-show/footer.php
+++ wp-content/themes/wedding-show/footer.php
@@ -20,15 +20,20 @@
 	    						    <?php joints_footer_links(); ?>
 	    					      </nav>
                                 </div>
-                            <div class="cell small-3"><p>TSB BANK <br />AREA</p></div>
+                            <div class="cell small-3">
+                                <p>Join us on:</p>
+                                <ul>
+                                    <li><a href="#">Facebook</a></li>
+                                    <li><a href="#">Instagram</a></li>
+                                </ul></div>
                         
                             <div class="cell small-3">
                             <p>Paula Bevege<br />
                                Phone: 0274 490 025<br />
-                               Email: paula@scenewgtn.co.nz</p>
+                               Email: paula@sceneevents.co.nz</p>
                             <p>Charlotte Wood<br />
                                Phone 027 372 5015<br />
-                               Email: charlotte@scenewgtn.co.nz</p>
+                               Email: charlotte@sceneevents.co.nz</p>
                             </div>
                         
                           
--- wp-content/themes/wedding-show/page-show.php
+++ wp-content/themes/wedding-show/page-show.php
@@ -11,15 +11,15 @@ get_header(); ?>
         <section class="cover-show">
             <div class="grid-container">
                <div class="grid-x grid-margin-x showdetails">
-                    <div class="cell small-4 "><p>1ST MAY 2022</p></div>
-                   <div class="cell small-4 medium-offset-4"><p>10AM - 4PM</p></div>
+                    <div class="cell medium-4 "><p>1ST MAY 2022</p></div>
+                   <div class="cell medium-4 medium-offset-4"><p>10AM - 4PM</p></div>
               </div>
                                <div class="grid-x grid-margin-x showdetails">
                    <div class="cell small-12"><h1>SCENE<br /> WEDDING &<br /> EVENT<br /> SHOW</h1></div>
                    </div>
                 <div class="grid-x grid-margin-x">
-                    <div class="cell small-4"><p>TSB BANK AREA</p></div>
-                    <div class="cell small-4 medium-offset-4"><p>WELLINGTON</p></div>
+                    <div class="cell medium-4"><p>TSB BANK AREA</p></div>
+                    <div class="cell medium-4 medium-offset-4"><p>WELLINGTON</p></div>
               </div>
             </div>
             
@@ -54,18 +54,18 @@ get_header(); ?>
               <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/show-1.jpg">
             </div>
             <div class="small-12 large-6 medium-6 cell">
-              <p class="info-box">Join us for a delicious day of fun, inspiration and the opportunity to meet the best and most creative wedding and event vendors Wellington has to offer.</p>
+              <p class="info-box">As the world is changing at a huge pace – the way we connect, and the interactions we have are also changing. As humans we crave genuine connections and experiences. </p>
             </div>
         </div> <!-- end #inner-content -->
     </div><!-- end grid-container --> 
     
     <div class="grid-container padding-row">
-        <div class="inner-content grid-x grid-margin-x grid-padding-x align-middle">
-            <div class="small-12 large-6 medium-6 cell">
-              <p class="info-box">Join us for a delicious day of fun, inspiration and the opportunity to meet the best and most creative wedding and event vendors Wellington has to offer.</p>
+        <div class="inner-content grid-x grid-margin-x grid-padding-x align-middle ">
+            <div class="small-12 large-6 medium-6 cell small-order-2 medium-order-1">
+              <p class="info-box">Experiences that bring us together and provide opportunities to celebrate and engage, whether for work or pleasure – personal growth or professional.</p>
             </div>
             
-            <div class="small-12 large-6 medium-6 cell">
+            <div class="small-12 large-6 medium-6 cell small-order-1 medium-order-2">
               <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/show-2.jpg">
             </div>
             
