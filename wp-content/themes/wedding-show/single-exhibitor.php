fd90c9d8286a6e2868834a06023ab90c220e9bce
iraritchiemeek <iraritchiemeek@gmail.com>
2021-11-05 11:52:15 +1300

    add custom fields to single exhibitor page

--- wp-content/themes/wedding-show/single-exhibitor.php
+++ wp-content/themes/wedding-show/single-exhibitor.php
@@ -1,7 +1,15 @@
 <?php 
 /*
-	Template Name: listing
+	Template Name: Single Exhibitor
 */
+
+$hours = get_field('hours');
+$phone = get_field('phone');
+$address = get_field('address');
+$email = get_field('email');
+$website = get_field('website');
+$facebook = get_field('facebook');
+$instagram = get_field('instagram');
 
 get_header(); ?>
 	
@@ -42,12 +50,13 @@ get_header(); ?>
                <div class="grid-x grid-margin-x details">                    
                    <div class="cell small-6 medium-2">                    
                       <h6>DETAILS</h6>
-                        <p>Hours: by appointment</p>
-                        <p>Phone: Show Number</p>
-                        <p>Address: Tauherenikau Racecourse Rd, State Highway 2, Tauherenikau 5771</p>
-                        <p>Email: Message Vendo===-098
-                        <p>Website: <a href="https://roseandsmith.co.nz/" target="_blank">roseandsmith.co.nz</a></p>
-                       <p>Facebook:  <a href="#">@roseandsmith</a></p>
+                        <?php echo $hours ? "<p>Hours: {$hours}</p>" : "" ?>
+                        <?php echo $phone ? "<p>Phone: {$phone}</p>" : "" ?>
+                        <?php echo $address ? "<p>Address: {$address}</p>" : "" ?>
+                        <?php echo $email ? "<p>Email: {$email}</p>" : "" ?>
+                        <?php echo $website ? "<p>Website: <a href='{$website}' target='_blank'>{$website}</a></p>" : "" ?>
+                        <?php echo $facebook ? "<p>Facebook: {$facebook}</p>" : "" ?>
+                        <?php echo $instagram ? "<p>Instagram: @{$instagram}</p>" : "" ?>
                     </div>
                    <div class="cell small-6 medium-2 medium-offset-1">                    
                       <h6>NOTES</h6>
